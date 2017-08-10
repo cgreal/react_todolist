02eb61463a129a93a4e0b218f5491aa91be84e05
cgreal <erol_cagri@hotmail.com>
2017-08-10 22:20:59 +0300

    Used ES2016 Property Initializer Syntax in ES6 classes and error message reviewed

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import './App.css';
 import { TodoForm, TodoList } from './components/todo'
 import { addTodo, generateId } from './lib/todoHelpers'
 
-
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
+  state = {
       todos: [
         { id: 1, name: 'öğren', isComplete: true },
         { id: 2, name: 'geliştir', isComplete: false },
@@ -16,38 +13,43 @@ class App extends Component {
       ],
       currentTodo: ''
     }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-  
-  handleInputChange(evt) {
+
+  handleInputChange = (evt) => {
     this.setState({
       currentTodo: evt.target.value
     })
   }
 
-  handleSubmit(evt) {
+  handleSubmit = (evt) =>{
     evt.preventDefault()
     const newId = generateId()
     const newTodo = { id: newId, name: this.state.currentTodo, isComplete: false }
     const updatedTodos = addTodo(this.state.todos, newTodo)
     this.setState({
       todos: updatedTodos,
-      currentTodo: ''
+      currentTodo: '',
+      errorMessage: ''
     })
   }
-
+  handleEmptySubmit = (evt) =>{
+    evt.preventDefault()
+      this.setState({
+        errorMessage: 'Lütfen bir todo girin!'
+      })
+  }
   render() {
+    const submitHandler = this.state.currentTodo ? this.handleSubmit : this.handleEmptySubmit
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Yapış listesi !</h2>
         </div>
-        <div className="todo-app">
+        <div className="Todo-App">
+          {this.state.errorMessage && <span className='error'>{this.state.errorMessage}</span>}
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
-            handleSubmit={this.handleSubmit} />
+            handleSubmit={submitHandler} />
 
           <TodoList todos={this.state.todos} />
 
